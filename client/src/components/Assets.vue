8beee115d821123461e99f6c11f5f78987d06ffe
Naskacz <milosz.hart@gmail.com>
2025-04-24 08:36:25 +0200

    Basic style for Assets.vue

--- client/src/components/Assets.vue
+++ client/src/components/Assets.vue
@@ -1,9 +1,27 @@
 <template>
-<div id="app">
-    <multiselect v-model="chosenAsset" :options="listOfAssets" :show-labels="true"></multiselect>
-    <input type ="number" v-model="balance" class="nb"/>
-    <button @click="handleAssetAdd" class="nb">Add</button>
-</div>
+    <div class="assets">
+        <div class="assetsHeader">
+            <a href="#"> Akcje </a>
+            <a href="#"> Obligacje </a>
+            <a href="#"> Surowce </a>
+        </div>
+        <div class = "assetsContent">
+            <div class = "assetsInputs">
+                <div class="wrapper">
+                    <div class="inputDecorator">
+                        <label for="balanceInput">Balans</label>
+                        <input type ="number" v-model="balance" class="nb" id="balanceInput" placeholder="Balans"/>
+                    </div>
+                        <div class="supporting-text"> Tekst pomocniczy dla balans</div>      
+                </div>     
+            <multiselect v-model="chosenAsset" :options="listOfAssets" :show-labels="true" id="assetSelect"></multiselect>
+            </div>
+            <div class="assetsButtons">
+                <button id="cancel"> Anuluj </button>
+                <button @click="handleAssetAdd" id="apply">Zatwierdź</button>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
     import Multiselect from 'vue-multiselect'
@@ -41,16 +59,96 @@
 </script>
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
-.nb{
-    height: 40px;
-    width: 40px;
+
+
+.assets{
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+}
+.assetsHeader{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items:center;
+    background-color:bisque;
+    padding: 20px 220px;
+        @media screen and (max-width: 1024px) {
+        padding: 20px 100px;      
+    }
+}
+.assetsHeader a{
+  color:brown;
+  text-decoration: none;
+}
+.assetsContent{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+}
+.assetsInputs{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 210px;
+    margin: 50px;
+}
+.assetsInputs input{
+    font-size: 30px;
+}
+.inputDecorator{
+    height: 56px;
+    width: 210px;
     padding: 5px;
     border: 1px solid #ccc;
-    border-radius: 4px;
+    background-color: rgb(230, 224, 233);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
 }
-.nb::-webkit-inner-spin-button, 
-.nb::-webkit-outer-spin-button {
+.wrapper{
+    display: flex;
+    flex-direction: column;
+    width:210px;
+    margin: 20px;
+}
+.supporting-text{
+    color:grey;
+    font-size: 12px;
+    margin-top: 5px;
+    margin-left: 5px;
+}
+.nb{
+    background-color: transparent;
+    border: none;
+    width: 200px;
+    height: 31px;
+}
+.nb::-webkit-outer-spin-button,
+.nb::-webkit-inner-spin-button {
   -webkit-appearance: none;
   margin: 0;
+}
+label{
+    color:dimgrey;
+    width: 210px;
+    height: 20px;
+}
+.assetsButtons button{
+    padding: 5px 10px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 20px;
+}
+#apply{
+    color:white;
+    background-color: black;
+}
+#cancel{
+    color:black;
+    background-color: white; 
 }
 </style>
