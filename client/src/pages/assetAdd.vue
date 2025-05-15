824ef99b510ab03fb864ccf435c47a5a2c24005a
Naskacz <milosz.hart@gmail.com>
2025-05-15 11:21:15 +0200

    Basic ui for adding assets using vuetify components

---
+++ client/src/pages/assetAdd.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-app-bar>
+        <v-container class="d-flex justify-space-between">
+            <a href="#"> Akcje </a>
+            <a href="#"> Obligacje </a>
+            <a href="#"> Surowce </a>
+        </v-container>
+    </v-app-bar>
+    <v-container class="d-flex flex-column align-center justify-center w-50" style="min-height: calc(80vh - 64px);">
+        <v-container class="d-flex flex-column ">
+            <v-select
+            v-model="chosenAsset"
+            label="Select"
+            :items="listOfAssets"
+            ></v-select>
+            <v-number-input
+            v-model="balance"
+                :reverse="false"
+                controlVariant="default"
+                label="Balance"
+                :precision="2"
+                :min="0"
+                :default="0.00"></v-number-input>
+        </v-container>    
+        <v-container class="d-flex justify-end">
+            <v-btn @click="handleAssetAdd"> Dodaj </v-btn>
+        </v-container>
+    </v-container>
+    
+</template>
+<script>
+    import { ref } from 'vue'
+    import axios from 'axios'
+    export default{
+        setup(){
+            const chosenAsset = ref('bronze');
+            const listOfAssets = ref(['bronze','silver','gold','platinum','diamond']);
+            const balance = ref(null);
+            function handleAssetAdd(){
+                const newAsset = {
+                    type: chosenAsset.value,
+                    balance: parseFloat(balance.value)
+                }
+                    axios.post('http://localhost:8000/asset/add', newAsset, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => {
+                        window.alert(`Dodano aktywo: ${chosenAsset.value} o wartości ${balance.value}`);
+                    })
+                    .catch(error => {
+                        window.alert(`Błąd: ${error.response ? error.response.data : error.message}`);
+                    });
+                }
+            return {chosenAsset, listOfAssets, balance, handleAssetAdd}
+    }
+}
+
+</script>
+
+<style>
+
+
+</style>
+
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,24 +1,35 @@
-import { createRouter, createWebHistory } from "vue-router";
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
 
-const Wallet = () => import('@/components/Wallet.vue')
-const Assets = () => import('@/components/Assets.vue')
-
-const routes = [
-    {
-        path: '/',
-        name: 'Wallet',
-        component: Wallet
-    },
-    {
-        path: '/assets',
-        name: 'Assets',
-        component: Assets
-    }
-]
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+    if (!localStorage.getItem('vuetify:dynamic-reload')) {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    } else {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
 })
 
 export default router
