0834b4035fee50307111e0b9322b9ad97e186b6c
Naskacz <milosz.hart@gmail.com>
2025-05-21 10:50:17 +0200

    Linted the code

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <v-list-item linkt subtitle="Vuetify" title="My Application" to="/" />
       <v-divider />
       <v-list-item link title="Wallet" to="/wallet" />
-      <v-list-item link title="Add Asset"to="/assetadd" />
+      <v-list-item link title="Add Asset" to="/assetadd" />
     </v-navigation-drawer>
   </v-container>
 </template>
